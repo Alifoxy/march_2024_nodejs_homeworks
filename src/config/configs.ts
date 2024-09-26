3f9bd75283be0dd842c656286d9ee048b3386e89
Alifoxy <nightfoxy666@gmail.com>
2024-09-26 20:02:04 +0300

    master

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -7,8 +7,12 @@ export const configs = {
     APP_HOST: process.env.APP_HOST,
 
     MONGO_URI: process.env.MONGO_URI,
+
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
     JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
     JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION,
+
+    SMTP_EMAIL: process.env.SMTP_EMAIL,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
 };
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -22,7 +22,7 @@ class AuthMiddleware {
                 TokenTypeEnum.ACCESS,
             );
 
-            const pair = await tokenRepository.findByParams({ accessToken });
+            const pair = await tokenRepository.findByParams({accessToken});
             if (!pair) {
                 throw new ApiError("Token is not valid", 401);
             }
@@ -32,6 +32,37 @@ class AuthMiddleware {
             next(e);
         }
     }
+
+    public async checkRefreshToken(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    )
+        {
+            try {
+                const header = req.headers.authorization;
+                if (!header) {
+                    throw new ApiError("Token is not provided", 401);
+                }
+                const refreshToken = header.split("Bearer ")[1];
+                const payload = tokenService.verifyToken(
+                    refreshToken,
+                    TokenTypeEnum.REFRESH,
+                );
+
+                const pair = await tokenRepository.findByParams({refreshToken});
+                if (!pair) {
+                    throw new ApiError("Token is not valid", 401);
+                }
+                req.res.locals.jwtPayload = payload;
+                req.res.locals.refreshToken = refreshToken;
+                next();
+            } catch (e) {
+                next(e);
+            }
+        }
+
 }
+
 
 export const authMiddleware = new AuthMiddleware();
