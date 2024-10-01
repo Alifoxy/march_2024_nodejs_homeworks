16da90f8a3fd0b0f2f345e83f74878a879261f7d
Alifoxy <nightfoxy666@gmail.com>
2024-10-01 14:59:42 +0300

    master

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -12,15 +12,25 @@ router.post(
     commonMiddleware.isBodyValid(UserValidator.create),
     authController.signUp,
 );
+
 router.post(
     "/sign-in",
     // commonMiddleware.isBodyValid(UserValidator.signIn),
     authController.signIn,
 );
+
 router.post(
     "/refresh",
     authMiddleware.checkRefreshToken,
     authController.refresh,
+);
+
+router.post("/logout", authMiddleware.checkAccessToken, authController.logout);
+
+router.post(
+    "/logout/all",
+    authMiddleware.checkAccessToken,
+    authController.logoutAll,
 );
 
 export const authRouter = router;
